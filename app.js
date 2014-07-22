bec6b81c3c411617da2370d9e908f1c504fa38f9
SanaNasar <sanasehereen.cse@gmail.com>
2014-07-22 16:49:35 -0700

    Working code to display list

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 var express = require("express");
+var request = require("request"); //Declaring the request requiring the request
 
 var app = express();
 
+//Directs to the index page or the homepage
 app.get('/', function(req, res){
   res.render('index.ejs');
+});
+
+//Directs to the search page using form action
+app.get('/search', function(req, res){
+	// res.send('search page reached!');//response on the search page
+	var query = req.query.searchTerm;
+  // res.send("search page: " + query);
+
+	var url = "http://www.omdbapi.com/?s=" + query;
+	request(url, function (error, response, body) {
+		if (!error) {
+			// res.send("response recieved: " + body);
+		var data = JSON.parse(body); //Coverting JSON data into javascript
+		res.render("results.ejs", {movieList: data.Search || [] });
+		}
+	});
+
 });
 
 app.listen(3000);
